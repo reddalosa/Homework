9f9221a554f7781b4a6f272f0e23aee7c249d32a
reddalosa <43438308+reddalosa@users.noreply.github.com>
2018-11-04 22:55:56 +0700

    Create guessingGame.js

---
+++ guessingGame.js
@@ -0,0 +1,21 @@
+const randomNumber = Math.floor(Math.random () *9 + 2);
+console.log(randomNumber);
+
+var promptValue = prompt(`Guess a Number between 1-10`);
+var num = parseFloat(promptValue);
+
+for (Attempt = 0; Attempt < 10; Attempt++) {
+  if (Attempt == 9) {
+    return alert(`You had 10 tries. GAMEOVER!`);
+  }
+	if (num == randomNumber) {
+  	return alert(`Congratulations, you are a mind reader!`);
+  }
+  if (num > randomNumber) {
+  	promptValue = prompt(`Your guess is higher. Guess a lower number.`); 
+  }
+  if(num < randomNumber) {
+  	promptValue = prompt(`Your guess is lower. Guess a higher number.`);
+  }
+  num = parseFloat(promptValue)
+}
